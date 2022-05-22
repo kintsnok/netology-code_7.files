778d1e3ebb26999f3a79d5186a54d58f6a3f6179
kintsnok <kintsnok@yandex.ru>
2022-05-22 11:24:14 +0300

    tests and comments

--- work_with_files.py
+++ work_with_files.py
@@ -7,7 +7,9 @@ from pprint import pprint
 # >>>         print(line, end='')
 # This final approach is more Pythonic and can be quicker and more memory efficient.
 
+# Задача №1
 def read_cook_book(file_path):
+    '''Считывает файл книги рецептов и создаёт словарь требуемого вида'''
     dish_dict = {}
     with open(file_path, encoding='utf-8') as f:
         for line in f:
@@ -29,7 +31,9 @@ def read_cook_book(file_path):
             dish_dict[dish_name] = ing_list
     return dish_dict
 
+# Задача №2
 def get_shop_list_by_dishes(dishes, person_count):
+    '''Создаёт словарь с названием ингредиентов и их количества для блюда'''
     a_cook_book = read_cook_book('recipes.txt')
     ing_dict = {}
     for dish in dishes:
@@ -45,7 +49,7 @@ def get_shop_list_by_dishes(dishes, person_count):
                     }
     return ing_dict
 
-
+# Задача №3
 def sorted_merge(sorted_dir_path, merged_file_path):
     files_info = []
     try:
@@ -70,9 +74,35 @@ def sorted_merge(sorted_dir_path, merged_file_path):
                     mf.write('\n')
 
 
-sorted_dir_path = os.path.join(os.getcwd(), 'sorted')
-merged_file_path = os.path.join(os.getcwd(), 'merged.txt')
-sorted_merge(sorted_dir_path, merged_file_path)
-with open(merged_file_path, 'r') as mf:
-    print(mf.read())
-os.remove(merged_file_path)
+def task1_test():
+    print('- Тест №1 -------------------------------------------------------------------')
+    cook_book = read_cook_book('recipes.txt')
+    print('cook_book =')
+    pprint(cook_book, indent=2, width=160)
+
+def task2_test():
+    print('- Тест №2 -------------------------------------------------------------------')
+    print('Запеченный картофель и Омлет на две персоны')
+    shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+    pprint(shop_list, indent=2, width=160)
+    print()
+    print('Фахитос Омлет на две персоны. Помидоров должно быть восемь')
+    shop_list = get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
+    pprint(shop_list, indent=2, width=160)
+
+def task3_test():
+    print('- Тест №3 -------------------------------------------------------------------')
+    sorted_dir_path = os.path.join(os.getcwd(), 'sorted')  # Путь к директории с файлами для сортировки
+    merged_file_path = os.path.join(os.getcwd(), 'merged.txt')  # Путь к создаваемому файлу слияния
+    sorted_merge(sorted_dir_path, merged_file_path)
+    with open(merged_file_path, 'r') as mf:
+        print(mf.read())  # Вывод содержимого файла слиняния
+    os.remove(merged_file_path)  # Удаляю созданный файл слияния
+
+
+# Тесты
+task1_test()
+print()
+task2_test()
+print()
+task3_test()
